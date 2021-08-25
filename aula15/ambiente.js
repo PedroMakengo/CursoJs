6bffe13fe76319f8faa33f0ddcea8144219817c2
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-25 09:09:06 -0300

    Vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+let num = [4, 5, 6, 7, 8];
+
+// Contar o vetor
+console.log(num.length);
+
+// Order o vetor
+console.log(num.sort());
+
+// Apresentar o vetor
+for (let i = 0; i <= num.length; i++) {
+  console.log(num[i]);
+}
---
+++ aula15/vetornatela.js
@@ -0,0 +1,14 @@
+let valores = [8, 1, 7, 4, 2, 9];
+
+for (let pos = 0; pos < valores.pos; pos++) {
+  console.log(valores[pos]);
+}
+
+for (let valor in valores) {
+  console.log(valores[valor]);
+}
+
+// Procurar um determinado valor dentro do meu vetor
+// E retorna a chave do meu valor (posição)
+// Caso não encontrar irá retornar -1
+valores.indexOf(7);
