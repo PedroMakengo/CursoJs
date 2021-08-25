b6a0fedbe4beb1f2cc920a1d8f508769451f8995
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-25 15:43:15 -0300

    Terminando o exercicío

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -1,4 +1,5 @@
-let btn = document.querySelector("#adicionar");
+let btnAdicionar = document.querySelector("#adicionar");
+let btnFinalizar = document.querySelector("#finalizar");
 
 let num = document.querySelector("#number");
 
@@ -24,14 +25,39 @@ function inLista(n, l) {
   }
 }
 
-btn.addEventListener("click", function () {
+btnAdicionar.addEventListener("click", function () {
   if (isNumero(num.value) && !inLista(num.value, valores)) {
     valores.push(Number(num.value));
     lista.innerHTML = "";
     for (let valor in valores) {
-      lista.innerHTML += `<option value=${valores[valor]}>${valores[valor]}</option>`;
+      lista.innerHTML += `<option value="${valores[valor]}">${valores[valor]}</option>`;
     }
   } else {
     window.alert("Valor invalido ou já encontrado na lista");
   }
+});
+
+btnFinalizar.addEventListener("click", function () {
+  if (valores.length == 0) {
+    alert("Adicione valores antes de finalizar");
+  } else {
+    let maior = valores[0];
+    let menor = valores[0];
+    let soma = 0;
+    let media = 0;
+    for (let i in valores) {
+      soma += valores[i];
+      media = valores[i] / valores.length;
+      if (valores[i] > maior) maior = valores[i];
+      if (valores[i] < menor) menor = valores[i];
+    }
+    res.innerHTML = "";
+    res.innerHTML += `<p>A todo temos ${lista.length} números cadastros</p>`;
+    res.innerHTML += `<p>O maior valor digitado ${maior}</p>`;
+    res.innerHTML += `<p>O menor valor digitado ${menor}</p>`;
+    res.innerHTML += `<p>Somando todos os valores ${soma}</p>`;
+    res.innerHTML += `<p>A média de todos os valores ${media}</p>`;
+  }
+  num.value = "";
+  num.focus();
 });
